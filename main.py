3c0881717ed4800b47a023c325e4cd2c9d6153ab
graciellavl <graciellavl@users.noreply.github.com>
2022-02-03 08:03:33 +0700

    add logic playfair main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from email.policy import default
 import PySimpleGUI as sg                        # Part 1 - The import
 from pathlib import Path
+from playfair import playfair_cipher_decrypt, playfair_cipher_encrypt
+from playfair1 import *
 
 sg.theme('DarkAmber')
 
@@ -15,9 +17,9 @@ layout = [[sg.Text("Welcome")],     # Part 2 - The Layout
     sg.FileBrowse(file_types=(("TXT Files", "*.txt"), ("ALL Files", "*.*"))),
     sg.Button("Open"),
 ],
-    [sg.Text("Plain Text"), sg.Input(key='-PLAINTEXT-')],
-    [sg.Text("Key"), sg.Input(key='-KEY-')],
-    [sg.Text("Cipher Text"), sg.Input(key='-CIPHERTEXT-')],
+    [sg.Text("Plain Text"), sg.Input(key='PLAINTEXT')],
+    [sg.Text("Key"), sg.Input(key='KEY')],
+    [sg.Text("Cipher Text"), sg.Input(key='CIPHERTEXT')],
     [sg.Button('Encrypt'), sg.Button('Decrypt')]]
 
 # Create the window
@@ -37,12 +39,43 @@ while True:
                 with open(filename, "rt", encoding='utf-8') as f:
                     text = f.read()
                 print(text)
-                window.Element(key='-PLAINTEXT-').Update(text)
+                window.Element(key='PLAINTEXT').Update(text)
 
             except Exception as e:
                 print("Error: ", e)
-    
-    if event == sg.WIN_CLOSED:
+
+    if values[0]:
+        print("Vigenere")
+        if event == "Encrypt":
+            print("Encrypt")
+        elif event == "Decrypt":
+            print("Decrypt")
+
+    elif values[1]:
+        print("Extended Vigenere")
+        if event == "Encrypt":
+            print("Encrypt")
+        elif event == "Decrypt":
+            print("Decrypt")
+
+    elif values[2]:
+        print("Playfair Cipher")
+        if event == "Encrypt":
+            cipher = playfair_cipher_encrypt(values["KEY"], values["PLAINTEXT"])
+            window.Element(key='CIPHERTEXT').Update(cipher)
+
+        elif event == "Decrypt":
+            plain = playfair_cipher_decrypt(values["KEY"], values["CIPHERTEXT"])
+            window.Element(key='PLAINTEXT').Update(plain)
+
+    elif values[3]:
+        print("Engima")
+        if event == "Encrypt":
+            print("Encrypt")
+        elif event == "Decrypt":
+            print("Decrypt")
+
+    elif event == sg.WIN_CLOSED:
         break
 
 # Finish up by removing from the screen
