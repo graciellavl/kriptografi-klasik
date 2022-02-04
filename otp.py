5cb0a976a4673000252d023112052cb901f70b69
chintyaw <cwchintya@gmail.com>
2022-02-04 11:23:29 +0700

    add otp

--- otp.py
+++ otp.py
@@ -1,11 +1,13 @@
 def generatekey(n):
     # generate kunci dari file key.txt
 
-    with open("key.txt", "r") as keyin:
-        kunci = keyin.read()[:n]
+    with open("kriptografi-klasik/key.txt", "r") as keyin:
+        tmp1 = keyin.read()
+        kunci = tmp1[:n]
 
-    with open("key.txt", "w") as keyout:
-        keyout.write(keyin[n:])
+    with open("kriptografi-klasik/key.txt", "w") as keyout:
+        tmp2 = tmp1[n:]
+        keyout.write(tmp2)
 
     return kunci
 
@@ -21,6 +23,8 @@ def otp_encrypt(text):
 
     key = generatekey(len(text_clean))
 
+    # print(key)
+
     for j in key:
         if (ord(j) >= 65 and ord(j) <= 122):
             key_clean += j
@@ -32,6 +36,34 @@ def otp_encrypt(text):
     # Enkripsi
     hasil = ''
     for i in range (0, len(text_clean)):
-        hasil +=  chr(((ord(text_clean[i])-65) + (ord(key[i])-65))%26 + 65)
+        hasil +=  chr(((ord(text_clean[i])-65) + (ord(key_clean[i])-65))%26 + 65)
 
-    return (hasil, key_clean)
+    # Simpan ke file
+    with open("kriptografi-klasik/otpcipher.txt", "a") as file:
+        file.write(hasil)
+
+    with open("kriptografi-klasik/otpkey.txt", "a") as file:
+        file.write(key)
+
+    return hasil
+
+
+def otp_decrypt(text):
+    try:
+        with open("kriptografi-klasik/otpcipher.txt") as file:
+            i = file.read().index(text)
+    
+    except (ValueError):
+        raise ValueError("Cipher tidak ditemukan")
+
+    with open("kriptografi-klasik/otpkey.txt") as file:
+        key = file.read()[i:i+len(text)]
+
+    hasil = ''
+    for i in range (0, len(text)):
+        hasil += chr(((ord(text[i])-65) - (ord(key[i])-65))%26 + 65)
+
+    return hasil
+
+
+# print (otp_decrypt("XJVH"))
